e3af31ec3d8dd30c169a8d81fdb1191fbb2c5b7e
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-06-16 19:48:20 -0300

    Model Docente refactorizado

--- tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -2,7 +2,14 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Docente {
 	
 	//Se definen los atributos para esta clase
@@ -13,67 +20,67 @@ public class Docente {
 	private long telefono;
 	
 	//Constructo por defecto
-	public Docente() {
-		super();
-	}
-	
+//	public Docente() {
+//		super();
+//	}
+//	
 	//Constructor parametrizado
-	public Docente(short legajo, String nombre, String apellido, String email, long telefono) {
-		super();
-		this.legajo = legajo;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.telefono = telefono;
-	}
+//	public Docente(short legajo, String nombre, String apellido, String email, long telefono) {
+//		super();
+//		this.legajo = legajo;
+//		this.nombre = nombre;
+//		this.apellido = apellido;
+//		this.email = email;
+//		this.telefono = telefono;
+//	}
 
 	//Getters and Setters
-	public short getLegajo() {
-		return legajo;
-	}
-
-	public void setLegajo(short legajo) {
-		this.legajo = legajo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public long getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(long telefono) {
-		this.telefono = telefono;
-	}
+//	public short getLegajo() {
+//		return legajo;
+//	}
+//
+//	public void setLegajo(short legajo) {
+//		this.legajo = legajo;
+//	}
+//
+//	public String getNombre() {
+//		return nombre;
+//	}
+//
+//	public void setNombre(String nombre) {
+//		this.nombre = nombre;
+//	}
+//
+//	public String getApellido() {
+//		return apellido;
+//	}
+//
+//	public void setApellido(String apellido) {
+//		this.apellido = apellido;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public long getTelefono() {
+//		return telefono;
+//	}
+//
+//	public void setTelefono(long telefono) {
+//		this.telefono = telefono;
+//	}
 
 	//toString
-	@Override
-	public String toString() {
-		return "Docente [legajo=" + legajo + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
-				+ ", telefono=" + telefono + "]";
-	}
+//	@Override
+//	public String toString() {
+//		return "Docente [legajo=" + legajo + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+//				+ ", telefono=" + telefono + "]";
+//	}
 	
 
 }
