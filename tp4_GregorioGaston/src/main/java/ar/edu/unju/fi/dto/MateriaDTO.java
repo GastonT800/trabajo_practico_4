940e7d5515cb54ac247aeef7107457427700d663
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-07-06 23:23:21 -0300

    Validaciones MateriaDTO y trazas de Docente

--- tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -6,8 +6,7 @@ import ar.edu.unju.fi.model.Materia.Modalidad;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-//import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,31 +21,25 @@ public class MateriaDTO {
 	
 	private int codigo;
 	
-	//@NotEmpty(message = "Ingrese un Nombre.")
-	@NotBlank
+	@NotBlank(message = "Ingrese un Nombre.")
 	@Size(min = 5, max = 30, message = "Debe tener entre 5 y 30 caracteres.")
 	private String nombre;
 	
-	//@NotEmpty(message = "Ingrese el Curso.")
-	@NotBlank
+	@NotBlank(message = "Ingrese el Curso.")
 	@Size(min = 5, max = 20, message = "Debe tener entre 5 y 20 caracteres.")
 	private String curso;
 	
-	//@NotEmpty(message = "Debe de ingresar una cantidad de horas")
 	@Min(value=60, message= "Debe ingresar un número mayor o igual a 60")
 	@Max(value=90, message= "Debe ingresar un número menor o igual a 90")
 	private int cantidadHoras;
 	
-	//@NotEmpty(message = "Seleccione la modalidad de la cursada.")
-	//@NotBlank
+	@NotNull(message = "Selecciona un tipo de modalidad")
 	private Modalidad modalidad;
 	
-	//@NotEmpty(message = "Seleccione un Docente para esta Materia.")
-	//@NotBlank
+	@NotNull(message = "Selecciona un Docente")
 	private DocenteDTO docente;
 	
-	//@NotEmpty(message = "Seleccione la Carrera a la que pertenece esta Materia.")
-	//@NotBlank
+	@NotNull(message = "Selecciona una Carrera")
 	private CarreraDTO carrera;
 	
 	private boolean estado;
--- tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/service/imp/DocenteServicelmp.java
+++ tp4_GregorioGaston/src/main/java/ar/edu/unju/fi/service/imp/DocenteServicelmp.java
@@ -1,7 +1,10 @@
 package ar.edu.unju.fi.service.imp;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,7 @@ import ar.edu.unju.fi.service.IDocenteService;
 @Service("docenteServiceMysql")
 public class DocenteServicelmp implements IDocenteService {
 	
+	private static Logger logger = LoggerFactory.getLogger(AlumnoServiceImp.class);
 	
 	@Autowired
 	private DocenteRepository docenteRepository;
@@ -26,6 +30,10 @@ public class DocenteServicelmp implements IDocenteService {
 	@Override
 	public List<DocenteDTO> findAll() {
 		List<DocenteDTO> docentesDTO = docenteMapper.toDocenteDTOList(docenteRepository.findAll());
+		if(docentesDTO.isEmpty())
+			logger.warn("No se pudo cargar la lista de Docentes");
+		else 
+			logger.info("Se listo correctamente la lista de Docentes");
 		return docentesDTO;
 	}
 
@@ -33,6 +41,10 @@ public class DocenteServicelmp implements IDocenteService {
 	@Override
 	public DocenteDTO findById(int id) {
 		DocenteDTO docenteDTO = docenteMapper.toDocenteDTO(docenteRepository.findById(id).get());
+		if(docenteDTO != null)
+			logger.info("Se encontro el objeto alumno con legajo " + id);
+		else 
+			logger.error("No se encontro el objeto alumno con legajo " + id);
 		return docenteDTO;
 	}
 
@@ -40,22 +52,32 @@ public class DocenteServicelmp implements IDocenteService {
 	@Override
 	public Docente save(DocenteDTO docenteDTO) {
 		Docente docente = docenteRepository.save(docenteMapper.toDocente(docenteDTO));
+		if(docente != null)
+	    	logger.info("El objeto Docente se guardo con exito");
+	    else 
+	    	logger.error("EL objeto Docente no se puede guardar con exito");
 		return docente;
 	}
 
 	//DELETE
 	@Override
 	public void deleteById(int id) {
-		Docente docente = docenteRepository.findById(id).get();
-		docente.setEstado(false);
-		docenteRepository.save(docente);
+		Optional<Docente> optionalDocente = docenteRepository.findById(id);
+	    if (optionalDocente.isPresent()) {
+	        Docente docente = optionalDocente.get();
+	        docente.setEstado(false);
+	        docenteRepository.save(docente);
+	        logger.warn("El objeto Docente con legajo " + id + " se eliminó con éxito!");
+	    } else {
+	        logger.error("No se encontró el objeto Docente con legajo " + id);
+	    }
 	}
 
 	//UPDATE
 	@Override
 	public void edit(DocenteDTO docenteDTO) throws Exception {
 		docenteRepository.save(docenteMapper.toDocente(docenteDTO));
-		
+		logger.info("Se edito con exito el obejto Docente con legajo " + docenteDTO.getLegajo());
 	}
 
 }
